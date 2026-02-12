0191917c91347a4453e3ae531cf841012a7acdb7
rintuchowdory <chowdoryrintu60@gmail.com>
2026-02-12 01:28:25 +0000

    Add creative animated React UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="background">
+      <div className="floating circle1"></div>
+      <div className="floating circle2"></div>
+      <div className="floating circle3"></div>
+
+      <div className="card">
+        <div className="logos">
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="GitHub"
+            className="logo"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+            alt="React"
+            className="logo spin"
+          />
+        </div>
+
+        <h1>
+          GitHub Codespaces <span>♥</span> React
+        </h1>
+
+        <p>Edit <code>src/App.jsx</code> and save to reload.</p>
+
+        <button>Learn React 🚀</button>
+      </div>
     </div>
   );
 }
